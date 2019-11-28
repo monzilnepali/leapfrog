888f7a4fd67068bb05f1735c27f20b94ee3950e8
manjilnepali <manjilnepali98@gmail.com>
2019-11-28 10:43:41 +0545

    fixing change indicator

--- ast1/oopscript.js
+++ ast1/oopscript.js
@@ -1,4 +1,3 @@
-//
 function Carousel(images, IMAGE_WIDTH, imageWrapper, indicatorWrapper, slideNext, slidePrev) {
   this.currentSlideIndex = 0;
   this.images = images;
@@ -17,7 +16,13 @@ Carousel.prototype.createIndicator = function () {
     var liElement = document.createElement("li");
     liElement.id = "circle" + i;
     var iconElement = document.createElement("i");
-    iconElement.className = "fa fa-circle-o";
+    if(i==0){
+      iconElement.className = "fa fa-circle";
+
+    }else{
+      iconElement.className = "fa fa-circle-o";
+
+    }
     liElement.addEventListener("click", function () {
       changeSlide(this);
     });
@@ -31,10 +36,10 @@ Carousel.prototype.createIndicator = function () {
     console.log("indicator" + nextSlideIndex);
     if (nextSlideIndex < that.currentSlideIndex) {
       //left slidez
-      that.slideleft(nextSlideIndex);
+      that.animate(nextSlideIndex);
     } else {
       //right slide
-      that.slideRight(nextSlideIndex);
+      that.animate(nextSlideIndex);
     }
 
   }
@@ -44,39 +49,16 @@ Carousel.prototype.createIndicator = function () {
 
 //change indicator of carousel
 Carousel.prototype.changeIndicator = function (index) {
-  document.getElementById("circle" + this.currentSlideIndex).childNodes[0].className = "fa fa-circle-o";
+ 
+  console.log("index>>>>"+index)
+  var indicator=document.getElementById(this.indicatorWrapper).children;
+  //console.log(indicator[index].childNodes[0])
+  indicator[index].childNodes[0].className="fa fa-circle";
+  indicator[this.currentSlideIndex].childNodes[0].className="fa fa-circle-o";
+ // indicator.getElementById("circle" + this.currentSlideIndex).className = "fa fa-circle-o";
   //changing class name
-  document.getElementById("circle" + index).childNodes[0].className = "fa fa-circle";
+ // indicator.getElementById("circle" + index).className = "fa fa-circle";
 };
-
-
-//slide to left
-// Carousel.prototype.slideleft = function (nextSlideIndex) {
-//   var currentWrapper = this.currentSlideIndex * this.IMAGE_WIDTH;
-//   var nextWrapper = nextSlideIndex * this.IMAGE_WIDTH;
-//   console.log("currentwrapper>>" + currentWrapper);
-//   this.changeIndicator(nextSlideIndex);
-//   var intervalTime = setInterval(function () {
-//     if (nextSlideIndex == images.length - 1) {
-//       currentWrapper += 100;
-//     } else {
-//       currentWrapper -= 100;
-//     }
-//     document.getElementsByClassName("carousel-image-wrapper")[0].style.left = -currentWrapper + "px";
-//     console.log("currentwrapper>>" + currentWrapper);
-//     console.log("next wrapper" + nextWrapper);
-//     if (nextSlideIndex == images.length - 1) {
-//       if (currentWrapper >= nextWrapper) {
-//         clearInterval(intervalTime);
-//       }
-//     } else {
-//       if (currentWrapper <= nextWrapper) {
-//         clearInterval(intervalTime);
-//       }
-//     }
-//   }, 30);
-//   this.currentSlideIndex = nextSlideIndex;
-// };
 
 //slide
 Carousel.prototype.animate = function (nextSlideIndex) {
@@ -85,15 +67,16 @@ Carousel.prototype.animate = function (nextSlideIndex) {
   var nextWrapper = nextSlideIndex * this.IMAGE_WIDTH;
   var diff= Math.abs(currentWrapper-nextWrapper);
   var flag=(nextWrapper>currentWrapper)?true:false;//for left and right animate
-//  console.log("dif>>>>>>"+ diff);
+  console.log("dif>>>>>>"+ diff);
   var step=Math.abs(this.currentSlideIndex-nextSlideIndex)
  // console.log("Step"+step)
   var totalstep=diff/(this.speed*step);
- // console.log("total step"+totalstep)
+  console.log("total step"+totalstep)
  // console.log("from"+this.currentSlideIndex+"to"+nextSlideIndex)
   console.log("current wrapperid >>>>>"+this.currentSlideIndex)
   console.log("next wrapperid >>>>>"+nextSlideIndex)
-
+var wrapper=document.getElementsByClassName(this.imageWrapper)[0];
+var that=this;
   var intervalTime = setInterval(function () {
     if(flag){
       if (nextSlideIndex==0) {
@@ -104,26 +87,15 @@ Carousel.prototype.animate = function (nextSlideIndex) {
         currentWrapper += 100*step;
       }
     }else{
-      if (nextSlideIndex == this.images.length - 1) {
-        currentWrapper += 100;
+      if (nextSlideIndex == that.images.length - 1) {
+        currentWrapper += 100*step;
       } else {
-        currentWrapper -= 100;
+        currentWrapper -= 100*step;
       }
     }
-   
-    document.getElementsByClassName("carousel-image-wrapper")[0].style.left = -currentWrapper + "px";
-     console.log("currentwrapper-------->" + currentWrapper);
-    // console.log("next wrapper---------->" + nextWrapper);
-
-    // if (nextSlideIndex == 0) {
-    //   if (currentWrapper <= nextWrapper) {
-    //     clearInterval(intervalTime);
-    //   }
-    // } else {
-    //   if (currentWrapper >= nextWrapper) {
-    //     clearInterval(intervalTime);
-    //   }
-    // }
+   console.log("iamge wrapper"+wrapper)
+    wrapper.style.left = -currentWrapper + "px";
+     console.log("currentwrapper-------->" + currentWrapper);  
   }, 30);
 
   setTimeout(function(){ 
@@ -133,22 +105,25 @@ Carousel.prototype.animate = function (nextSlideIndex) {
 
   this.currentSlideIndex = nextSlideIndex;
 };
-
+//initalize the carousel
 Carousel.prototype.init = function () {
   //styling image
-  for (var i = 0; i < images.length; i++) {
-    images[i].style.left = this.IMAGE_WIDTH * i + "px";
+  for (var i = 0; i < this.images.length; i++) {
+    this.images[i].style.left = this.IMAGE_WIDTH * i + "px";
   }
   //create indicator
   this.createIndicator();
-  this.changeIndicator(this.currentSlideIndex);
-  //adding event handler for prev and next
+    //adding event handler for prev and next
   this.slidePrev.onclick = this.prevImage.bind(this);
   this.slideNext.onclick = this.nextImage.bind(this);
 };
+//set indicator
+Carousel.prototype.setIndicator=function(index){
+  this.changeIndicator(index);
+}
 //onlick slidePrev 
 Carousel.prototype.prevImage = function () {
-  var nextSlideIndex = (this.currentSlideIndex + images.length - 1) % this.images.length;
+  var nextSlideIndex = (this.currentSlideIndex + this.images.length - 1) % this.images.length;
   // this.slideleft(nextSlideIndex);
   this.animate(nextSlideIndex);
 }
@@ -160,8 +135,17 @@ Carousel.prototype.nextImage = function () {
 }
 
 //getting list images from dom
-var images = document.getElementsByTagName("img");
-var prevSlide = document.getElementById("prevSlide");
-var nextSlide = document.getElementById("nextSlide");
-var first = new Carousel(images, 1200, "carousel-image-wrapper", "carousel-indicator", nextSlide, prevSlide);
+var images1 = document.getElementsByClassName('carousel-image-wrapper1')[0].children;
+var prevSlide1 = document.getElementById("prevSlide1");
+var nextSlide1 = document.getElementById("nextSlide1");
+var first = new Carousel(images1, 1200, "carousel-image-wrapper1", "carousel-indicator1", nextSlide1, prevSlide1);
 first.init();
+
+
+var images2 = document.getElementsByClassName('carousel-image-wrapper2')[0].children;
+var prevSlide2 = document.getElementById("prevSlide2");
+var nextSlide2 = document.getElementById("nextSlide2");
+var second = new Carousel(images2, 1200, "carousel-image-wrapper2", "carousel-indicator2", nextSlide2, prevSlide2);
+second.init();
+
+
