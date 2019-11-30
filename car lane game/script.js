bb14469aaed342e54aae28beda4d8087e5518ce8
manjilnepali <manjilnepali98@gmail.com>
2019-11-30 12:25:10 +0545

    fixing road assets random

--- car lane game/script.js
+++ car lane game/script.js
@@ -10,11 +10,15 @@ Lane.prototype.create = function () {
   lanDiv.style.left = this.x + "px";
   lanDiv.style.top = this.y + "px";
   lanDiv.classList.add("road-asset");
+  //setting image of road
+  var randomImage = Math.floor(Math.random() * 2) + 1;
+  console.log(randomImage)
+  lanDiv.style.backgroundImage = "url(./images/assets/road-asset-" + randomImage + ".png";
   this.element = lanDiv
   this.parent.appendChild(lanDiv)
 }
 Lane.prototype.move = function () {
-  console.log("moved")
+  //console.log("moved")
   this.y += this.dy;
   this.draw();
 }
@@ -27,18 +31,16 @@ function Game(parentElement) {
   var height = 795;
 
   Game.prototype.init = function () {
-
     //crating five lane
-    for (var j = 0; j < 2; j++) {
-      //two lane
-      for (var i = 0; i < 3; i++) {
-        // var lane = new Lane(parentElement, 320 + 150 * j, (85 + 80) * i);
-        var lane = new Lane(parentElement, 0, (270 + 50) * i)
-        lane.create();
-        this.lane.push(lane);
-        // console.log("lane created")
-      }
+
+    for (var i = 0; i < 3; i++) {
+      // var lane = new Lane(parentElement, 320 + 150 * j, (85 + 80) * i);
+      var lane = new Lane(parentElement, 0, (270 + 50) * i)
+      lane.create();
+      this.lane.push(lane);
+      // console.log("lane created")
     }
+
     this.animateLane();
 
   }
