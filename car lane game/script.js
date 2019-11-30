04648cdcefcdacce1500a8b3a4d96f2dcd2b044f
manjilnepali <manjilnepali98@gmail.com>
2019-11-30 20:53:03 +0545

    add start screen and fixing car spawn

--- car lane game/script.js
+++ car lane game/script.js
@@ -5,12 +5,15 @@ var LANE_SHIFT = 72 * 2;
 var LEFT_LANE_POSITION = (157 + 72);
 var MIDDLE_LANE_POSITION = LEFT_LANE_POSITION + LANE_SHIFT;
 var RIGHT_LANE_POSITION = MIDDLE_LANE_POSITION + LANE_SHIFT;
-
+var FPS = 60;
+var UPDATE_Y = 10;
+var gameLoop = null;
+var gameStopFlag = true;
 
 function Road(parent, x, y) {
   this.x = x;
   this.y = y;
-  this.dy = 5;
+  this.dy = UPDATE_Y;
   this.element = null;
   this.parent = parent;
 }
@@ -21,15 +24,17 @@ document.addEventListener("keypress", function (event) {
 
   //a=97 d =100
   //for a event handling
-  if (event.keyCode == 97 && mainCar.currentLane != 1) {
-    console.log("moving to left")
-    mainCar.steerLeft();
-  }
-  if (event.keyCode == 100 && mainCar.currentLane != 3) {
-    console.log("moving to right")
-    mainCar.steerRight();
-  }
+  if (gameStopFlag) {
+    if (event.keyCode == 97 && mainCar.currentLane != 1) {
+      console.log("moving to left")
+      mainCar.steerLeft();
 
+    }
+    if (event.keyCode == 100 && mainCar.currentLane != 3) {
+      console.log("moving to right")
+      mainCar.steerRight();
+    }
+  }
 
 });
 
@@ -58,11 +63,13 @@ Road.prototype.draw = function () {
 function Car(parentElement, x, y, flag) {
   this.x = x;
   this.y = y;
+  this.width = 55;
+  this.height = 110;
   this.element = null;
   this.currentLane = 0;
   this.parent = parentElement;
   this.flag = flag || 0; //0 mean main car and 1 mean obstacle car
-  this.dy = 5;
+  this.dy = UPDATE_Y;
 }
 Car.prototype.createCar = function () {
   var car = document.createElement('div');
@@ -70,7 +77,8 @@ Car.prototype.createCar = function () {
     car.classList.add('main-car');
   } else {
     car.classList.add('other-car');
-    car.style.backgroundImage = "url(./images/assets/car-1.png)";
+    var randomCar = Math.floor(Math.random() * 5) + 1;
+    car.style.backgroundImage = 'url(./images/assets/car-' + randomCar + '.png)';
   }
   car.style.left = this.x + 'px';
   car.style.top = this.y + 'px';
@@ -79,33 +87,38 @@ Car.prototype.createCar = function () {
   return this;
 }
 
-Car.prototype.move = function () {
+Car.prototype.draw = function () {
 
   this.element.style.left = this.x + 'px';
   this.element.style.top = this.y + 'px';
 
 }
-Car.prototype.update = function () {
+Car.prototype.move = function () {
+
   this.y += this.dy;
-  this.move();
+  this.draw();
+}
+Car.prototype.moveTo = function (valuex) {
+  this.x = valuex;
+  this.draw();
 }
 
 Car.prototype.moveToMiddleLane = function () {
   this.x = MIDDLE_LANE_POSITION;
   this.currentLane = 2;
-  this.move();
+  this.draw();
 }
 
 Car.prototype.moveToLeftLane = function () {
   this.x = LEFT_LANE_POSITION;
   this.currentLane = 1;
-  this.move();
+  this.draw();
 }
 
 Car.prototype.moveToRightLane = function () {
   this.x = RIGHT_LANE_POSITION;
   this.currentLane = 3;
-  this.move();
+  this.draw();
 }
 
 
@@ -117,7 +130,7 @@ Car.prototype.steerLeft = function () {
   if (this.currentLane != 1) {
     this.x = this.x - LANE_SHIFT;
     this.currentLane = this.currentLane - 1;
-    this.move();
+    this.draw();
   }
 
 
@@ -129,7 +142,7 @@ Car.prototype.steerRight = function () {
   if (this.currentLane != 3) {
     this.x = this.x + LANE_SHIFT;
     this.currentLane = this.currentLane + 1;
-    this.move();
+    this.draw();
   }
 }
 
@@ -144,10 +157,14 @@ function Game(parentElement) {
     mainCar.moveToMiddleLane();
 
     //road asset creation
+
     for (var i = 0; i < 3; i++) {
       var lane = new Road(parentElement, 0, (270 + 50) * i)
       lane.create();
-      var car = new Car(parentElement, 20 * i, (270 + 50) * i, 1).createCar();
+
+      var car = new Car(parentElement, 0, (55 + 100) * i, 1).createCar();
+
+
       var rand = Math.floor(Math.random() * 3) + 1;
       console.log(rand)
       if (rand === 1) {
@@ -169,30 +186,61 @@ function Game(parentElement) {
   }
 }
 Game.prototype.animateLane = function () {
-
   var that = this;
-  console.log(this.otherCar)
-  var interval = setInterval(function () {
 
-
+  gameLoop = setInterval(function () {
     that.roadElements.forEach((roadElement, index) => {
-
-      //checking border for roadelement
-      if (roadElement.y >= 730) {
-        roadElement.y = -270;
-        that.otherCar[index].y = -270;
-        //updaing lane image
-        // var randomImage = Math.floor(Math.random() * 4) + 1;
-        // console.log(randomImage)
-        // roadElement.element.style.backgroundImage = "url(./images/assets/road-asset-" + randomImage + ".png";
+      //checking collision with main car
+      if (mainCar.x == that.otherCar[index].x) {
+        //check collision with other car in same line
+        console.log("same lane" + that.otherCar[index]);
+        that.checkCollision(that.otherCar[index]);
       }
-      that.otherCar[index].update();
+
+      //checking border for road element
+      //if exceed reset its position
+      if (roadElement.y >= 785) {
+        roadElement.y = -200; //reset y into intial position
+
+      }
+      if (that.otherCar[index].y >= 785) {
+        that.otherCar[index].y = -200;
+      }
+      that.otherCar[index].move(); //update position
       roadElement.move();
     });
-  }, 10);
+  }, 1000 / FPS);
+
+}
+Game.prototype.checkCollision = function (otherCar) {
+  console.log("check collision")
+  if (mainCar.x < otherCar.x + otherCar.width &&
+    mainCar.x + mainCar.width > otherCar.x &&
+    mainCar.y < otherCar.y + otherCar.height &&
+    mainCar.y + otherCar.height > otherCar.y) {
+    console.log("collision");
+    //terminate game
+
+    // if (mainCar.y != otherCar.y) {
+    //   if (otherCar.x < mainCar.x) {
+    //     mainCar.moveTo(otherCar.x + otherCar.width);
+    //   } else {
+    //     mainCar.moveTo(otherCar.x - otherCar.width);
+    //   }
+    // }
+
+    gameStopFlag = false;
+    //move to
+    clearInterval(gameLoop);
+  }
 
 }
 
 var parentElement = document.getElementById('app');
-var game = new Game(parentElement);
-game.init()
+var startElement = document.getElementById('start-btn');
+
+startElement.addEventListener('click', function () {
+  var game = new Game(parentElement);
+  startElement.parentElement.style.display = 'none';
+  game.init()
+});
