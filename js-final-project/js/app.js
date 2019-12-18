fcc7bd24b56f37cc8f755994092aef458f68f8e0
manjilnepali <manjilnepali98@gmail.com>
2019-12-19 01:05:38 +0545

    table update

--- js-final-project/js/app.js
+++ js-final-project/js/app.js
@@ -10,7 +10,3 @@ window.onload = function () {
   container.style.height = window.innerHeight + "px";
   new App();
 }
-document.getElementById('new').addEventListener('click', () => {
-  console.log("new tab")
-  new App()
-});
