56b7ad0f186240bb39cbe5db1018f4efc0a9f4e7
manjilnepali <manjilnepali98@gmail.com>
2019-12-19 23:13:56 +0545

    switch orientation

--- js-final-project/js/app.js
+++ js-final-project/js/app.js
@@ -1,44 +1,36 @@
 class App {
   constructor() {
-    new Editor(this.context);
+    this.editor = new Editor(this.context);
   }
-
 }
 
 window.onload = function () {
-  var container = document.getElementById('container');
+
+  let container = document.getElementById('container');
+  let rotateBtn = document.getElementById('rotateBtn');
   container.style.height = window.innerHeight + "px";
-  new App();
-  let example1Data = "Title:Atm Transaction Sequence Diagram" +
-    "\nuser->atm:card insert" +
-    "\natm->user:enter your pin code" +
-    "\nuser->atm:pin code send" +
-    "\natm->bank:check this details";
-
-  // let example1 = document.getElementById("example1");
-  //example1.addEventListener('click', () => {
-  //  console.log(this.exampleJson)
-  //   let element = document.getElementsByClassName('text-area')[0];
-  //   element.value = example1Data;
-  //   let event = new Event('input');
-  //   element.dispatchEvent(event);
-
-  // });
+  let wrapperRightContainter = document.getElementById('wrapper-right');
+  wrapperRightContainter.style.height = window.innerHeight + 'px';
+  wrapperRightContainter.style.width = (window.innerWidth * 0.68) + 'px';
+  let app = new App();
   let exampleElements = document.getElementsByClassName('example');
-  let textAreaEelement = document.getElementsByClassName('text-area')[0];
+  let textAreaElement = document.getElementsByClassName('text-area')[0];
+  let prevActiveElement = document.getElementsByClassName('active')[0];
   Array.from(exampleElements).forEach(element => {
     element.addEventListener('click', () => {
       //getting other active element
-      let prevActiveElement = document.getElementsByClassName('active')[0];
+
       if (prevActiveElement) {
-        prevActiveElement.className = 'element';
+        //removing active class from prev active element
+        prevActiveElement.classList.remove('active');
       }
       element.classList.add('active')
       let id = element.getAttribute('data-id');
       let data = this.exampleJson['example' + id].join('\n');
-      textAreaEelement.value = data;
+
+      textAreaElement.value = data;
       let event = new Event('input');
-      textAreaEelement.dispatchEvent(event);
+      textAreaElement.dispatchEvent(event);
     })
   });
 
@@ -46,6 +38,41 @@ window.onload = function () {
     // Parsing JSON string into object
     var actual_JSON = JSON.parse(response);
     this.exampleJson = actual_JSON;
+  });
+
+  rotateBtn.addEventListener('click', () => {
+    console.log("rotate")
+    //change orientation
+    let mainContainer = document.getElementsByClassName('container')[0];
+    let rotateFlag = rotateBtn.getAttribute('data-id');
+    let leftContainer = document.getElementById('editor-container');
+    let rightContainer = document.getElementById('wrapper-right');
+    if (rotateFlag == 0) {
+      //current rotate data=0 mean portrait mode
+      //change to landscape mode
+      mainContainer.style.flexDirection = 'column';
+      leftContainer.classList.add('landscape-orientation')
+      rightContainer.classList.add('landscape-orientation')
+      rotateBtn.setAttribute('data-id', 1);
+      wrapperRightContainter.style.width = (window.innerWidth) + 'px';
+      app.editor.resizeCanvas();
+      let event = new Event('input');
+      textAreaElement.dispatchEvent(event);
+    } else {
+      //current rotate data=1 mean landscape mode
+      //change to portait mode
+      mainContainer.style.flexDirection = 'row';
+      leftContainer.classList.remove('landscape-orientation')
+      rightContainer.classList.remove('landscape-orientation')
+      rotateBtn.setAttribute('data-id', 0);
+      wrapperRightContainter.style.width = (window.innerWidth * 0.68) + 'px';
+      app.editor.resizeCanvas();
+      let event = new Event('input');
+      textAreaElement.dispatchEvent(event);
+    }
+
+
+
   });
 
 }
