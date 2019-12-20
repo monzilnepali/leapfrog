5d08c9fbf3c4fbb4532d7dd0978c346ba2773291
manjilnepali <manjilnepali98@gmail.com>
2019-12-20 09:19:56 +0545

    addition condition in lexer

--- js-final-project/js/Lexer.js
+++ js-final-project/js/Lexer.js
@@ -103,7 +103,7 @@ class Lexer {
         console.log(this.token.length)
 
         if (this.token.length == 1) {
-          console.log("1 length")
+          // 1 length before : expected title keyword
           if (this.token[this.token.length - 1].type === 'token') {
             this.input.next();
             this.token.push({
@@ -114,10 +114,10 @@ class Lexer {
             this.token = [];
             this.parseFlag = true;
           } else {
-            throw new ValidationError(`unexpected '${ch}' at [${this.input.line},${this.input.col}]`);
+            throw new ValidationError(`Expected Title keyword  at [${this.input.line},${this.input.col-1}]`);
           }
         } else {
-          if (this.token[this.token.length - 2].type === 'arrow') {
+          if (this.token.length !== 0 && this.token[this.token.length - 2].type === 'arrow') {
             if (this.token[this.token.length - 1].type === 'actor') {
               this.parseFlag = true;
               this.token.push({
@@ -136,40 +136,11 @@ class Lexer {
               throw new ValidationError(`Actor expected at [${this.input.line},${this.input.col}]`);
             }
           } else {
-            throw new ValidationError(`unexpected at [${this.input.line},${this.input.col}]`);
+            throw new ValidationError(`unexpected ${ch} at [${this.input.line},${this.input.col}]`);
           }
         }
 
-        // if (this.token[this.token.length - 1].type === 'token') {
-        //   this.input.next();
-        //   this.token.push({
-        //     type: 'Title',
-        //     value: this.readMessage()
-        //   });
-        //   this.tempToken.push(this.token);
-        //   this.token = [];
-        //   this.parseFlag = true;
-        // } else if (this.token[this.token.length - 2].type === 'arrow') {
-        //   if (this.token[this.token.length - 1].type === 'actor') {
-        //     this.parseFlag = true;
-        //     this.token.push({
-        //       type: 'col',
-        //       value: this.input.next()
-        //     });
-        //     //reading string after :
-        //     this.token.push({
-        //       type: 'message',
-        //       value: this.readMessage()
 
-        //     });
-        //     this.tempToken.push(this.token);
-        //     this.token = [];
-        //   } else {
-        //     throw new ValidationError(`Actor expected at [${this.input.line},${this.input.col}]`);
-        //   }
-        // } else {
-        //   throw new ValidationError(`unexpected at [${this.input.line},${this.input.col}]`);
-        // }
 
       }
     } catch (error) {
